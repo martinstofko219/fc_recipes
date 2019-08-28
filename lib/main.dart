3f3f5d7c8dd989bf4fac574e847b8de100b873a1
Martin Stofko <martinstofko114@gmail.com>
2019-08-27 21:57:51 -0700

    Updated app fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.indigoAccent,
         canvasColor: Color.fromRGBO(245, 245, 245, 1),
-        fontFamily: 'Raleway',
+        fontFamily: 'SourceSansPro',
       ),
       home: CategoriesPage(),
     );
