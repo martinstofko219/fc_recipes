87bc2ec1c8b8d2473cc91ea3155c450ee1568968
Martin Stofko <martinstofko114@gmail.com>
2019-08-28 15:33:30 -0700

    Added dummy meal data and laid out the meals page.

---
+++ lib/models/meal.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+
+enum Complexity { Simple, Challenging, Hard }
+
+enum Affordability { Affordable, Pricey, Luxurious }
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String name;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int cookTime;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Meal({
+    @required this.id,
+    @required this.categories,
+    @required this.name,
+    @required this.imageUrl,
+    @required this.ingredients,
+    @required this.steps,
+    @required this.cookTime,
+    @required this.complexity,
+    @required this.affordability,
+    this.isGlutenFree = false,
+    this.isLactoseFree = false,
+    this.isVegan = false,
+    this.isVegetarian = false,
+  });
+}
--- lib/pages/meals_page.dart
+++ lib/pages/meals_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy-data.dart';
+
 class MealsPage extends StatelessWidget {
   static const routeName = '/category-meals';
 
@@ -7,14 +9,27 @@ class MealsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
     final categoryName = routeArgs['name'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryName),
       ),
-      body: Center(
-        child: Text('$categoryName meal list...'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final meal = categoryMeals[index];
+
+          return Card(
+            child: ListTile(
+              title: Text(meal.name),
+            ),
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
